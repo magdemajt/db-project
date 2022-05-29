f273b0a1a19f0e43395f414249c1f0251cacdbed
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2022-05-29 22:12:39 +0200

    Added info how to connect to testing database

--- reddit/dbConnection.ts
+++ reddit/dbConnection.ts
@@ -9,4 +9,10 @@ const db = new Pool({
   port: 5432,
 });
 
+// const db = new Pool({
+//   connectionString: 'postgres://iapjikgy:5M4e8o53dG-7h6s_k7FU-HNH9iBKI4M4@abul.db.elephantsql.com/iapjikgy'
+// });
+// Ewentualnie port 5432 jak nie zadziała
+
+
 export default db;
